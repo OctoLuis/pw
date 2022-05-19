a848f0d0b96a78a2608703775d14f603b4141b47
OctoLuis <73768287+OctoLuis@users.noreply.github.com>
2022-05-19 02:33:38 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const images = gsap.utils.toArray('img');
+
+const showDemo = () => {
+  document.body.style.overflow = 'auto';
+
+
+  // (w.scrollWidth - section.offsetWidth) * -1]
+  gsap.utils.toArray('section').forEach((section, index) => {
+    const w = section.querySelector('.wrapper');
+    const [x, xEnd] = index % 2 ? ['100%', 0] : [w.scrollWidth * 1, 0];
+    gsap.fromTo(w, { x }, {
+      x: xEnd,
+      scrollTrigger: {
+        trigger: section,
+        scrub: 5 } });
+
+
+  });
+};
+
+imagesLoaded(images).on('always', showDemo);
