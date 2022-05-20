d689dc5d93d87767e61f210a07f7fafca5393f8d
OctoLuis <73768287+OctoLuis@users.noreply.github.com>
2022-05-20 04:21:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const showDemo = () => {
   // (w.scrollWidth - section.offsetWidth) * -1]
   gsap.utils.toArray('section').forEach((section, index) => {
     const w = section.querySelector('.wrapper');
-    const [x, xEnd] = index % 2 ? ['-100%', '80%'] : [w.scrollWidth * 1, 0];
+    const [x, xEnd] = index % 2 ? ['-100%', '60%'] : [w.scrollWidth * 1, 0];
     gsap.fromTo(w, { x }, {
       x: xEnd,
       scrollTrigger: {
